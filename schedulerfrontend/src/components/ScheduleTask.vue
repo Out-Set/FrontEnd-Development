68b8f3c0ab975e76be3dbda4ebb2f960e83cfaba
savan-bitsflow <savan.prajapati@bitsflowtech.com>
2023-11-07 13:13:52 +0530

    Scheduler frontend Completed

--- schedulerfrontend/src/components/ScheduleTask.vue
+++ schedulerfrontend/src/components/ScheduleTask.vue
@@ -8,31 +8,37 @@
       <div style="display: flex;">
         <strong>Second:</strong>&nbsp;
         <select v-model="second" style="width:150px; margin-bottom: 20px; margin-right: 20px;">
+          <option value="*" disabled selected>*</option>
           <option v-for="second in seconds" :key="second" :value="second">{{ second }}</option>
         </select>
 
         <strong>Minute:</strong>&nbsp;
         <select v-model="minute" style="width:150px; margin-bottom: 20px; margin-right: 20px;">
+          <option value="*" disabled selected>*</option>
           <option v-for="minute in minutes" :key="minute" :value="minute">{{ minute }}</option>
         </select>
 
         <strong>Hours:</strong>&nbsp;
         <select v-model="hour" style="width:150px; margin-right: 20px; margin-bottom: 20px; ">
+          <option value="*" disabled selected>*</option>
           <option v-for="hour in hours" :key="hour" :value="hour">{{ hour }}</option>
         </select>
 
         <strong>Date:</strong>&nbsp;
         <select v-model="date" style="width:150px; margin-bottom: 20px; margin-right: 20px;">
+          <option value="*" disabled selected>*</option>
           <option v-for="date in dates" :key="date" :value="date">{{ date }}</option>
         </select>
 
         <strong>Month:</strong>&nbsp;
         <select v-model="month" style="width:150px; margin-bottom: 20px; margin-right: 20px;">
+          <option value="*" disabled selected>*</option>
           <option v-for="month in months" :key="month" :value="month">{{ month }}</option>
         </select>
 
         <strong>Day:</strong>&nbsp;
         <select v-model="day" style="width:150px; margin-bottom: 20px; margin-right: 20px;">
+          <option value="*" disabled selected>*</option>
           <option v-for="day in days" :key="day" :value="day">{{ day }}</option>
         </select>
       </div>
@@ -42,12 +48,16 @@
         </button>
       </div>
 
-      <div>
+      <div style="padding-top: 30px;">
         <strong>Write Your cron Expression</strong>&nbsp;
         <input type="text" v-model="expression">&nbsp;
+        <button ref="cron-btn" class="reschedule-bt" v-on:click="setCronExp()">Set Schedule</button>&nbsp;&nbsp;
 
-        <button ref="cron-btn" class="reschedule-bt" v-on:click="fun()">Set Schedule</button>
+        <strong>Enter Delay in MilliSecond</strong>&nbsp;
+        <input type="number" v-model="delay">&nbsp;
+        <button ref="cron-btn" class="reschedule-bt" v-on:click="setDelay()">Set Delay</button>
       </div>
+
     </div>
 
   </div>
@@ -62,25 +72,26 @@ export default {
   data() {
     return {
       seconds: ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59'],
-      second: '00',
+      second: '*',
 
       minutes: ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '50', '51', '52', '53', '54', '55', '56', '57', '58', '59'],
-      minute: '00',
+      minute: '*',
 
       hours: ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'],
-      hour: '00',
+      hour: '*',
 
       dates: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'],
-      date: '01',
+      date: '*',
 
       months: ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
-      month: 'JAN',
+      month: '*',
 
       days: ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'],
-      day: 'MON',
+      day: '*',
 
       selectedData: "",
       expression: "",
+      delay: 0
     }
   },
 
@@ -93,13 +104,8 @@ export default {
     console.log("Date: ", this.date);
     console.log("Month: ", this.month);
     console.log("Day: ", this.day);
-
-
-    // disabled select interval, if only one job is selected
-    if (this.selectInterval > 1) {
-      // this.$refs.interval.disabled = false
-    }
   },
+
   watch: {
 
     second(newSecond) {
@@ -133,28 +139,38 @@ export default {
     },
 
   },
-  computed: {
-    getJobsFromLS() {
-      let jobs = JSON.parse(localStorage.getItem('jobs'));
-      return jobs;
-    },
-
-    selectInterval() {
-      let jobs = JSON.parse(localStorage.getItem('jobs'));
-      return jobs.length;
-    }
-  },
 
   methods: {
     set() {
       console.log(this.second, this.minute, this.hour, this.date, this.month, this.day);
       this.selectedData = this.second + " " + this.minute + " " + this.hour + " " + this.date + " " + this.month + " " + this.day
       console.log(typeof this.selectedData, this.selectedData);
+
+      axios.post('http://localhost:8081/dynamicSchedule/cronVal', this.selectedData)
+        .then((response) => {
+          console.log("Response form Backend: ", response);
+        })
+        .catch((error) => {
+          // Handle the error
+          console.log("Error Occured!", error);
+        });
     },
 
-    fun() {
+    setCronExp() {
       console.log(typeof this.expression, this.expression);
-      axios.post('http://localhost:8081/expression', eval(this.expression))
+      axios.post('http://localhost:8081/dynamicSchedule/cronVal', this.expression)
+        .then((response) => {
+          console.log("Response form Backend: ", response);
+        })
+        .catch((error) => {
+          // Handle the error
+          console.log("Error Occured!", error);
+        });
+    },
+
+    setDelay(){
+      console.log(typeof this.delay, this.delay);
+      axios.get('http://localhost:8081/dynamicSchedule/delayVal?delay='+this.delay)
         .then((response) => {
           console.log("Response form Backend: ", response);
         })
